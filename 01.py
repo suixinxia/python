65d7c45177bacd760dd31a31a78a7dec73750241
suixinxia <2139418372@qq.com>
2025-07-26 13:50:26 +0800

    Add files via upload

---
+++ 01.py
@@ -0,0 +1,27 @@
+i = 1
+# 定义一个框变量：
+sum = 0
+while i <= 100:
+    
+
+    sum += i #注意加的是i
+    i += 1
+print(f'sum = {sum}')
+
+
+
+count = 2
+while True:
+    if count % 2==1 and count % 3== 1 and count % 4 == 1 :
+        print(f'count={count}')
+        break
+    count += 1
+
+
+
+
+
+
+
+
+
